655d2895ef700a9c6d17e12183d0f37ce96950fd
Antoine Stjepanovic <apbstep@gmail.com>
2020-11-03 23:50:21 +0100

    commit

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
 	validates :start_date, presence: true
 	has_many :attendances 
 	has_many :users, through: :attendances
+
+	def ending_date
+		end_date = start_date + duration*60
+	end
 end
